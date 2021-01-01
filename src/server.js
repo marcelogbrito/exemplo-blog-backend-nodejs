3db81babbf6e1f63192d870d9722a4cf30a37223
Marcelo Brito <marcelogbrito@gmail.com>
2021-01-01 18:24:23 -0300

    endpoints api de curtidas e comentarios implementadas (sem persistencia ainda)

--- src/server.js
+++ src/server.js
@@ -4,12 +4,15 @@ import bodyParser from "body-parser"
 const informacoesArtigo = {
 "aprenda-react" : {
     curtidas: 0,
+    comentarios: [],
 },
 "aprenda-node" : {
     curtidas: 0,
+    comentarios: [],
 },
 "my-thoughts-on-resumes" : {
     curtidas: 0,
+    comentarios: [],
 }
 }
 
@@ -17,8 +20,16 @@ const app = express()
 
 app.use(bodyParser.json())
 
-app.get('/hello',(req, res) => res.send('Hello!'))
-app.get('/hello/:nome',(req, res) => res.send(`Hello ${req.params.nome}!`))
-app.post('/hello',(req, res) => res.send(`Hello! ${req.body.nome}`))
+app.post('/api/artigos/:nome/curtida', (req, res) => {
+    const nomeArtigo = req.params.nome;
+    informacoesArtigo[nomeArtigo].curtidas += 1;
+    res.status(200).send(`${nomeArtigo} agora tem ${informacoesArtigo[nomeArtigo].curtidas} curtidas`);
+})
+app.post("/api/artigos/:nome/novo-comentario", (req, res) => {
+    const {nomeUsuario, texto} = req.body;
+    const nomeArtigo = req.params.nome;
+    informacoesArtigo[nomeArtigo].comentarios.push({nomeUsuario, texto});
+    res.status(200).send(informacoesArtigo[nomeArtigo]);
+})
 
 app.listen(8001, () => console.log('Escutando na porta 8001'))
