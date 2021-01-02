9f8f7531fb9073fff10403c10435afc461d3482d
Marcelo Brito <marcelogbrito@gmail.com>
2021-01-01 21:52:43 -0300

    implementado endpoint 'comentarios' com persistencia pelo mongodb

--- src/server.js
+++ src/server.js
@@ -6,17 +6,15 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/api/artigos/:nome", async (req, res) => {
+const withDb = async (operations, res) => {
   try {
-    const nomeArtigo = req.params.nome;
     const client = await MongoClient.connect("mongodb://localhost:27017", {
       useNewUrlParser: true,
     });
     const db = client.db("meu-blog");
-    const informacoesArtigo = await db
-      .collection("artigos")
-      .findOne({ nome: nomeArtigo });
-    res.status(200).json(informacoesArtigo);
+
+    await operations(db);
+
     //fecha conexao do banco de dados
     client.close();
   } catch (error) {
@@ -24,41 +22,68 @@ app.get("/api/artigos/:nome", async (req, res) => {
       .status(500)
       .json({ message: "erro na conexao ao banco de dados: ", error });
   }
-});
+};
 
-app.post("/api/artigos/:nome/curtida", async (req, res) => {
-  try {
+app.get("/api/artigos/:nome", async (req, res) => {
+  withDb(async (db) => {
     const nomeArtigo = req.params.nome;
-    const client = await MongoClient.connect("mongodb://localhost:27017", {
-      useNewUrlParser: true, useUnifiedTopology: true 
-    });
-    const db = client.db("meu-blog");
     const informacoesArtigo = await db
       .collection("artigos")
       .findOne({ nome: nomeArtigo });
-    await db.collection("artigos").updateOne({
-      nome: nomeArtigo},{
-      '$set': {
-        curtidas: informacoesArtigo.curtidas + 1,
+    res.status(200).json(informacoesArtigo);
+  }, res);
+});
+
+app.post("/api/artigos/:nome/curtida", async (req, res) => {
+  withDb(async (db) => {
+    const nomeArtigo = req.params.nome;
+    const informacoesArtigo = await db
+      .collection("artigos")
+      .findOne({ nome: nomeArtigo });
+    await db.collection("artigos").updateOne(
+      {
+        nome: nomeArtigo,
       },
-    });
+      {
+        $set: {
+          curtidas: informacoesArtigo.curtidas + 1,
+        },
+      }
+    );
     const informacoesArtigoAtualizada = await db
       .collection("artigos")
       .findOne({ nome: nomeArtigo });
 
     res.status(200).json(informacoesArtigoAtualizada);
-    client.close();
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "erro na conexao ao banco de dados: ", error });
-  }
+  }, res);
 });
 app.post("/api/artigos/:nome/novo-comentario", (req, res) => {
   const { nomeUsuario, texto } = req.body;
   const nomeArtigo = req.params.nome;
-  informacoesArtigo[nomeArtigo].comentarios.push({ nomeUsuario, texto });
-  res.status(200).send(informacoesArtigo[nomeArtigo]);
+  withDb(async (db) => {
+    const informacoesArtigo = await db
+      .collection("artigos")
+      .findOne({ nome: nomeArtigo });
+    await db.collection("artigos").updateOne(
+      {
+        nome: nomeArtigo,
+      },
+      {
+        $set: {
+          comentarios: informacoesArtigo.comentarios.concat({
+            nomeUsuario,
+            texto,
+          }),
+        },
+      }
+    );
+
+    const informacoesArtigoAtualizada = await db
+      .collection("artigos")
+      .findOne({ nome: nomeArtigo });
+
+    res.status(200).json(informacoesArtigoAtualizada);
+  }, res);
 });
 
 app.listen(8001, () => console.log("Escutando na porta 8001"));
